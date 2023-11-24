b34aad718e3a24b88f5c15260d5c0bd3e0e69d22
Mikołaj Wasilewski <waselland@gmail.com>
2023-11-24 21:57:31 +0100

    Updated player Movement

--- Assets/Scripts/Characters/Movement/CharacterMover.cs
+++ Assets/Scripts/Characters/Movement/CharacterMover.cs
@@ -8,6 +8,11 @@ public class CharacterMover : MonoBehaviour
     private Transform groundCheck;
     private LayerMask groundMask;
 
+    private bool isCrouching;
+    private bool isSprinting;
+    private bool isWalking;
+
+
     private void Awake ()
     {
         controller = GetComponent<CharacterController>();
@@ -20,7 +25,38 @@ public class CharacterMover : MonoBehaviour
         controller.Move(moveVector * Time.deltaTime);
     }
 
-    public bool isGrounded {
+    public void Crouch (){
+        // TODO
+    }
+
+    public void Sprint (){
+
+    }
+
+    public void Walk (){
+
+    }
+
+    public bool IsGrounded {
         get { return controller.isGrounded || Physics.CheckSphere(groundCheck.position, 1f, groundMask); }
     }
+
+    public bool IsCrouching {
+        get { return isCrouching; }
+        set { isCrouching = value; }
+    }
+
+    public bool IsSprinting {
+        get { return isSprinting; }
+        set { isSprinting = value; }
+    }
+
+    public bool IsWalking {
+        get { return isWalking; }
+        set { isWalking = value; }
+    }
+}
+
+public enum MoveDirection{
+    Forwards, Backwards, Left, Right
 }
--- Assets/Scripts/Characters/Movement/PlayerMovement.cs
+++ Assets/Scripts/Characters/Movement/PlayerMovement.cs
@@ -1,20 +1,162 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : CharacterMover
 {
-    private Vector3 currentMovementVector;
+    [Header ("Current Movement")]
+    private Dictionary<MoveDirection, bool> moveStatus;
+    private Vector3 adjustedMovementVector;
+    private Vector3 movementVector;
+    private float currentSpeed;
+    private float speedMultiplier;
+    private bool ignoreNextSprintInput;
 
+    [Header ("Movement settings")]
+    [SerializeField] private const float WalkingSpeed = 4f;
+    [SerializeField] private const float RunningSpeed = 10f;
+    [SerializeField] private const float SprintingSpeed = 16f;
+    [SerializeField] private const float CrouchingSpeedMultiplier = .4f;
+    [SerializeField] private const float DefaultSpeedMultiplier = 1f;
+
+
+    private void Awake ()
+    {
+        currentSpeed = RunningSpeed;
+
+        KeyboardInput input = InputManager.GetKeyboardHandle();
+
+        // Forwards Movement
+        input.onButtonDown["Move Forwards"] += ChangeForwardMovementState;
+        input.onButtonUp["Move Forwards"] += ChangeForwardMovementState;
+
+        // Backwards Movement
+        input.onButtonDown["Move Backwards"] += ChangeBackwardsMovementState;
+        input.onButtonUp["Move Backwards"] += ChangeBackwardsMovementState;
+
+        // Left Movement
+        input.onButtonDown["Move Left"] += ChangeLeftMovementState;
+        input.onButtonUp["Move Left"] += ChangeLeftMovementState;
+
+        // Right Movement
+        input.onButtonDown["Move Right"] += ChangeRightMovementState;
+        input.onButtonUp["Move Right"] += ChangeRightMovementState;
+    }
     
     private void Update()
     {
         
     }
 
-    private void UpdateHorizontalMovement (){
+    private void UpdateHorizontalMovement ()
+    {
         
     }
 
+    #region Movement
 
+    private void ChangeForwardMovementState ()
+    {
+        Vector3 directionalMovement = new Vector3(0f, 0f, 1f) * currentSpeed;
+        if (moveStatus[MoveDirection.Forwards]) 
+        {
+            movementVector -= directionalMovement;
+        } 
+        else 
+        {
+            movementVector += directionalMovement;
+        }
+
+        moveStatus[MoveDirection.Forwards] = !moveStatus[MoveDirection.Forwards];
+    }
+
+    private void ChangeBackwardsMovementState ()
+    {
+        Vector3 directionalMovement = new Vector3(0f, 0f, -1f) * currentSpeed;
+        if (moveStatus[MoveDirection.Backwards]) 
+        {
+            movementVector -= directionalMovement;
+        } 
+        else 
+        {
+            movementVector += directionalMovement;
+        }
+
+        moveStatus[MoveDirection.Backwards] = !moveStatus[MoveDirection.Backwards];
+    }
+
+    private void ChangeLeftMovementState ()
+    {
+        Vector3 directionalMovement = new Vector3(-1f, 0f, 0f) * currentSpeed;
+        if (moveStatus[MoveDirection.Left]) 
+        {
+            movementVector -= directionalMovement;
+        } 
+        else 
+        {
+            movementVector += directionalMovement;
+        }
+
+        moveStatus[MoveDirection.Left] = !moveStatus[MoveDirection.Left];
+    }
+
+    private void ChangeRightMovementState ()
+    {
+        Vector3 directionalMovement = new Vector3(1f, 0f, 0f) * currentSpeed;
+        if (moveStatus[MoveDirection.Right]) 
+        {
+            movementVector -= directionalMovement;
+        } 
+        else 
+        {
+            movementVector += directionalMovement;
+        }
+
+        moveStatus[MoveDirection.Right] = !moveStatus[MoveDirection.Right];
+    }
+
+    #endregion
+
+    private void ChangeCrouchStatus ()
+    {
+        IsCrouching = !IsCrouching;
+        if (IsCrouching)
+        {
+            speedMultiplier = CrouchingSpeedMultiplier;
+        }
+        else
+        {
+            speedMultiplier = DefaultSpeedMultiplier;
+        }
+        Crouch();
+    }
+
+    private void ChangeSprintStatus ()
+    {
+        IsSprinting = !IsSprinting;
+        if (IsSprinting)
+        {
+            currentSpeed = SprintingSpeed;
+        }
+        else
+        {
+            currentSpeed = RunningSpeed;
+        }
+        Sprint();
+    }
+
+    private void ChangeWalkStatus ()
+    {
+        IsWalking = !IsWalking;
+        if (IsWalking)
+        {
+            currentSpeed = WalkingSpeed;
+        }
+        else
+        {
+            currentSpeed = RunningSpeed;
+        }
+        Walk();
+    }
 }
