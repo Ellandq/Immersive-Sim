4e630d271e6cd3cc791b86975187685b3bbc4f3a
Mikołaj Wasilewski <waselland@gmail.com>
2024-06-07 20:59:53 +0200

    Added new assets

---
+++ Assets/Scripts/Characters/CharacterAnimationStateHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationStateHandler : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    
+    
+}
+
+public enum MovementState
+{
+    
+}
